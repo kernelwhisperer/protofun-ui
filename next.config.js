6852dc77c5b71b078ac12c2569d214dce143fc3d
Daniel Constantin <hello@danielconstantin.net>
2023-06-23 21:59:36 +0300

    Integrate Sentry.io

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+// Injected content via Sentry wizard below
+
+import { withSentryConfig } from "@sentry/nextjs";
+
 const nextConfig = {
   modularizeImports: {
     "@mui/icons-material": {
@@ -17,3 +21,34 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+module.exports = withSentryConfig(
+  module.exports,
+  {
+    org: "coldpizza",
+
+    project: "protofun",
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options
+    // Suppresses source map uploading logs during build
+    silent: true,
+  },
+  {
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
+
+    // Transpiles SDK to be compatible with IE11 (increases bundle size)
+    transpileClientSDK: false,
+
+    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+    tunnelRoute: "/monitoring",
+
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
+  }
+);
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  dsn: "https://672b2daf06ae4f9d8ca9956097e75502@o4505410061795328.ingest.sentry.io/4505410080931840",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+});
