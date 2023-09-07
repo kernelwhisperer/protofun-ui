989ac3bd421f60555423a7d1d4d7851c5f319959
Daniel Constantin <hello@danielconstantin.net>
2023-09-07 12:13:19 +0300

    Update api config

--- app/api/feathers-app.ts
+++ app/api/feathers-app.ts
@@ -3,7 +3,7 @@ import socketio from "@feathersjs/socketio-client";
 import { createClient } from "protofun-service";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:3030");
+const socket = io(process.env.NEXT_PUBLIC_PROTOFUN_SERVICE as string);
 const app = createClient(socketio(socket));
 app.configure(auth());
 
