ca10cb7f2e9a6d39d02ca9b1483fda3c358b85c1
Daniel Constantin <hello@danielconstantin.net>
2023-09-19 19:34:09 +0300

    Add mark all notifs as read

--- app/components/RootLayout/Notifications/NotifPanel.tsx
+++ app/components/RootLayout/Notifications/NotifPanel.tsx
@@ -1,6 +1,8 @@
+import { MarkEmailReadOutlined } from "@mui/icons-material"
 import {
   avatarClasses,
   Box,
+  Button,
   List,
   ListItem,
   listItemAvatarClasses,
@@ -15,6 +17,7 @@ import Decimal from "decimal.js"
 import Link from "next/link"
 import { useSearchParams } from "next/navigation"
 import { useSnackbar } from "notistack"
+import { METRICS_MAP, PROTOCOL_MAP } from "protofun"
 import { Notification } from "protofun-service"
 import React, { useCallback } from "react"
 
@@ -22,7 +25,6 @@ import { Alert } from "../../../api/alerts-api"
 import { $notifications, archiveNotification } from "../../../api/notifications-api"
 import { formatNumber, logError, PopoverToggleProps } from "../../../utils/client-utils"
 import { RobotoMonoFF } from "../../Theme/fonts"
-import { METRICS_MAP, PROTOCOL_MAP } from "protofun"
 
 function formatValue(alert: Alert) {
   const metric = METRICS_MAP[alert.protocolId][alert.metricId]
@@ -60,6 +62,17 @@ export function NotifPanel({ toggleOpen }: Pick<PopoverToggleProps, "toggleOpen"
     [enqueueSnackbar, toggleOpen]
   )
 
+  const handleMarkAll = useCallback(() => {
+    $notifications.get().forEach((notification) => {
+      archiveNotification(notification).catch((error: Error) => {
+        logError(error)
+        enqueueSnackbar(`Error: ${error.message}`, {
+          variant: "error",
+        })
+      })
+    })
+  }, [enqueueSnackbar])
+
   return (
     <>
       {notifications.length === 0 && (
@@ -86,7 +99,8 @@ export function NotifPanel({ toggleOpen }: Pick<PopoverToggleProps, "toggleOpen"
           },
         })}
       >
-        {notifications.map((notification) => {
+        {/* TODO */}
+        {notifications.slice(0, 20).map((notification) => {
           const notifDatetime = new Date(notification.createdAt * 1000)
           const notifDateLabel = formatDistance(notifDatetime, new Date(), {
             addSuffix: true,
@@ -174,6 +188,25 @@ export function NotifPanel({ toggleOpen }: Pick<PopoverToggleProps, "toggleOpen"
           )
         })}
       </List>
+      {notifications.length > 0 && (
+        <Button
+          sx={{
+            "&:not(:hover) > span": {
+              opacity: 0.5,
+            },
+            background: "var(--mui-palette-secondary-main) !important",
+            bottom: 0,
+            position: "absolute",
+            transform: "none !important",
+          }}
+          size="small"
+          fullWidth
+          endIcon={<MarkEmailReadOutlined fontSize="small" />}
+          onClick={handleMarkAll}
+        >
+          <span>Mark all notifications as read</span>
+        </Button>
+      )}
     </>
   )
 }
